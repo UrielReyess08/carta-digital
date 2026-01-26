8320fc9e59dcf7de8c0f77010e5c60e2ba4e5f4b
UrielReyess08 <sj77447994@zegelipae.pe>
2026-01-26 13:20:38 -0500

    se agrego coreccion a los productos + fotos de los productos

--- lib/menu-data.ts
+++ lib/menu-data.ts
@@ -32,38 +32,39 @@ export const menuData: MenuItem[] = [
   { id: "trago-lumumba-latte-12", name: "Trago Lumumba", price: 10.00, category: "SIN CAFÉ 12oz", image: "/TRAGO LUMUMBA.webp"},
 
   // FRAPPUCCINOS 12oz
-  { id: "frap-cafe-12", name: "Café", price: 9.00, category: "FRAPPUCCINOS 12oz" },
-  { id: "frap-caramel-12", name: "Caramel", price: 9.00, category: "FRAPPUCCINOS 12oz" },
-  { id: "frap-moca-12", name: "Moca", price: 9.00, category: "FRAPPUCCINOS 12oz" },
-  { id: "frap-matcha-12", name: "Matcha", price: 10.00, category: "FRAPPUCCINOS 12oz", image: "/FRAPPUCINO MATCHA.webp" },
+  { id: "frap-cafe-12", name: "Frappuccino Café", price: 9.00, category: "FRAPPUCCINOS 12oz" },
+  { id: "frap-caramel-12", name: "Frappuccino Caramel", price: 9.00, category: "FRAPPUCCINOS 12oz" },
+  { id: "frap-moca-12", name: "Frappuccino Moca", price: 9.00, category: "FRAPPUCCINOS 12oz" },
+  { id: "frap-matcha-12", name: "Frappuccino Matcha", price: 10.00, category: "FRAPPUCCINOS 12oz", image: "/FRAPPUCINO MATCHA.webp" },
 
   // SMOOTHIES 12oz
-  { id: "smooth-fresa-12", name: "Fresa", price: 9.00, category: "SMOOTHIES 12oz", image: "/SMOOTHIES DE FRESA.webp"},
-  { id: "smooth-lucuma-12", name: "Lúcuma", price: 9.00, category: "SMOOTHIES 12oz", image: "/SMOOTHIES DE LUCUMA.webp"},
-  { id: "smooth-mango-12", name: "Mango", price: 9.00, category: "SMOOTHIES 12oz", image: "/SMOOTHIES DE MANGO.webp"},
-  { id: "smooth-arandano-12", name: "Arándano", price: 9.00, category: "SMOOTHIES 12oz", image: "/SMOOTHIES DE ARANDANO.webp"},
-  { id: "smooth-frambuesa-12", name: "Frambuesa", price: 11.00, category: "SMOOTHIES 12oz" },
+  { id: "smooth-fresa-12", name: "Smoothie Fresa", price: 9.00, category: "SMOOTHIES 12oz", image: "/SMOOTHIES DE FRESA.webp"},
+  { id: "smooth-lucuma-12", name: "Smoothie Lúcuma", price: 9.00, category: "SMOOTHIES 12oz", image: "/SMOOTHIES DE LUCUMA.webp"},
+  { id: "smooth-mango-12", name: "Smoothie Mango", price: 9.00, category: "SMOOTHIES 12oz", image: "/SMOOTHIES DE MANGO.webp"},
+  { id: "smooth-arandano-12", name: "Smoothie Arándano", price: 9.00, category: "SMOOTHIES 12oz", image: "/SMOOTHIES DE ARANDANO.webp"},
+  { id: "smooth-frambuesa-12", name: "Smoothie Frambuesa", price: 11.00, category: "SMOOTHIES 12oz" },
 
   // FROZEN 12oz
-  { id: "frozen-fresa-12", name: "Fresa", price: 8.00, category: "FROZEN 12oz", image: "/FROZEN DE FRESA.webp" },
-  { id: "frozen-lucuma-12", name: "Lúcuma", price: 8.00, category: "FROZEN 12oz", image: "/FROZEN DE LUCUMA.webp" },
-  { id: "frozen-mango-12", name: "Mango", price: 8.00, category: "FROZEN 12oz", image: "/FROZEN DE MANGO.webp" },
-  { id: "frozen-arandano-12", name: "Arándano", price: 8.00, category: "FROZEN 12oz", image: "/FROZEN DE ARANDANO.webp" },
-  { id: "frozen-maracuya-12", name: "Maracuyá", price: 8.00, category: "FROZEN 12oz", image: "/FROZEN DE MARACUYA.webp" },
-  { id: "frozen-frambuesa-12", name: "Frambuesa", price: 10.00, category: "FROZEN 12oz", image: "/FROZEN DE FRAMBUESA.webp" },
+  { id: "frozen-fresa-12", name: "Frozen Fresa", price: 8.00, category: "FROZEN 12oz", image: "/FROZEN DE FRESA.webp" },
+  { id: "frozen-lucuma-12", name: "Frozen Lúcuma", price: 8.00, category: "FROZEN 12oz", image: "/FROZEN DE LUCUMA.webp" },
+  { id: "frozen-mango-12", name: "Frozen Mango", price: 8.00, category: "FROZEN 12oz", image: "/FROZEN DE MANGO.webp" },
+  { id: "frozen-arandano-12", name: "Frozen Arándano", price: 8.00, category: "FROZEN 12oz", image: "/FROZEN DE ARANDANO.webp" },
+  { id: "frozen-maracuya-12", name: "Frozen Maracuyá", price: 8.00, category: "FROZEN 12oz", image: "/FROZEN DE MARACUYA.webp" },
+  { id: "frozen-frambuesa-12", name: "Frozen Frambuesa", price: 10.00, category: "FROZEN 12oz", image: "/FROZEN DE FRAMBUESA.webp" },
 
   // SNACKS
-  { id: "snack-sandwich-pollo", name: "Sándwich – Pollo al horno", price: 6.00, category: "SNACKS" },
-  { id: "snack-sandwich-tocino", name: "Sándwich – Tocino ahumado", price: 6.00, category: "SNACKS" },
-  { id: "snack-crepe-vainilla", name: "Crepe – Vainilla", price: 4.00, category: "SNACKS" },
-  { id: "snack-crepe-maracuya", name: "Crepe – Maracuyá", price: 4.00, category: "SNACKS" },
-  { id: "snack-kanelbullar", name: "Kanelbullar (rollo de canela sueco)", price: 4.00, category: "SNACKS" },
-  { id: "snack-chokladbollar", name: "Chokladbollar (trufas de chocolate sueco)", price: 2.00, category: "SNACKS" },
+  { id: "snack-sandwich-pollo", name: "Sándwich – Pollo al horno", price: 6.00, category: "SNACKS", image: "/Pollo al horno.webp" },
+  { id: "snack-sandwich-tocino", name: "Sándwich – Tocino ahumado", price: 6.00, category: "SNACKS", image: "/Tocino ahumado .webp" },
+  { id: "snack-crepe-vainilla", name: "Crepe – Vainilla", price: 4.00, category: "SNACKS", image: "/Crepes de Vainilla.webp" },
+  { id: "snack-crepe-maracuya", name: "Crepe – Maracuyá", price: 4.00, category: "SNACKS", image: "/Crepes de Maracuya.webp" },
+  { id: "snack-crepe-fresa", name: "Crepe - Fresa", price: 4.00, category: "SNACKS", image: "/Crepes de Fresa.webp"},
+  { id: "snack-kanelbullar", name: "Kanelbullar", price: 4.00, description: "Rollo de canela sueco", category: "SNACKS", image: "/Kanelbullar.webp" },
+  { id: "snack-chokladbollar", name: "Chokladbollar", price: 2.00, description: "Trufas de chocolate sueco",  category: "SNACKS", image: "/Chokladbollar.webp" },
 
   // ADICIONALES
-  { id: "adic-shot-baileys", name: "Shot de Baileys", price: 3.00, category: "ADICIONALES" },
-  { id: "adic-shot-ron", name: "Shot de Ron", price: 2.00, category: "ADICIONALES" },
-  { id: "adic-crema", name: "Crema", price: 2.00, category: "ADICIONALES" },
+  { id: "adic-shot-baileys", name: "Shot de Baileys", price: 3.00, category: "ADICIONALES", image: "/Shot de Baileys.webp" },
+  { id: "adic-shot-ron", name: "Shot de Ron", price: 2.00, category: "ADICIONALES", image: "/Shot de Ron.webp" },
+  { id: "adic-crema", name: "Crema", price: 2.00, category: "ADICIONALES", image: "/crema.webp" },
 ]
 
 export const getCategoriesInOrder = (): string[] => {
