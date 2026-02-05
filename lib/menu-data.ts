7fc7fdca6cf8ca7ce392fec535ac08b7ed754e88
UrielReyess08 <sj77447994@zegelipae.pe>
2026-02-05 00:36:47 -0500

    se agrego nuevo formato de fotos + nuevo formato de la carta

--- lib/menu-data.ts
+++ lib/menu-data.ts
@@ -10,14 +10,14 @@ export interface MenuItem {
 export const menuData: MenuItem[] = [
 
   // CAFÉ 7oz
-  { id: "cafe-vikingo-baileys-7", name: "Vikingo Baileys Coffee", price: 6.00, category: "CAFÉ 7oz", description: "Espresso + Baileys + crema", image: "/VikingoBaileysCoffee.webp" },
+  { id: "cafe-vikingo-baileys-7", name: "Vikingo Baileys Coffee", price: 6.00, category: "CAFÉ 7oz", image: "/Cafe7/VikingoBaileysCoffee.webp" },
   //{ id: "cafe-vikingo-bombom-7", name: "Vikingo Bom bóm", price: 5.00, category: "CAFÉ 7oz", description: "Espresso + Ron + crema" },
 
   // CAFÉ 10oz
-  { id: "cafe-americano-10", name: "Americano", price: 3.00, category: "CAFÉ 10oz", image: "/americano.webp"},
-  { id: "cafe-espresso-10", name: "Espresso", price: 3.00, category: "CAFÉ 10oz", image: "/Espresso.webp" },
-  { id: "cafe-cappuccino-10", name: "Cappuccino", price: 4.00, category: "CAFÉ 10oz", image: "/Capuccino.webp"},
-  { id: "cafe-mocaccino-10", name: "Mocaccino", price: 5.00, category: "CAFÉ 10oz", image: "/Mocaccino.webp"},
+  { id: "cafe-americano-10", name: "Americano", price: 3.00, category: "CAFÉ 10oz", image: "/Cafe/Americano.webp"},
+  { id: "cafe-espresso-10", name: "Espresso", price: 3.00, category: "CAFÉ 10oz", image: "/Cafe/Espresso.webp" },
+  { id: "cafe-cappuccino-10", name: "Cappuccino", price: 4.00, category: "CAFÉ 10oz", image: "/Cafe/Capuccino.webp"},
+  { id: "cafe-mocaccino-10", name: "Mocaccino", price: 5.00, category: "CAFÉ 10oz", image: "/Cafe/Mocaccino.webp"},
   //{ id: "cafe-winter-summer-10", name: "Winter / Summer Coffee", price: 4.00, category: "CAFÉ 10oz", description: "Americano + crema" },
   //{ id: "cafe-vikingo-power-10", name: "Vikingo Power Coffee", price: 5.00, category: "CAFÉ 10oz", description: "Doble espresso + crema" },
 
@@ -25,46 +25,47 @@ export const menuData: MenuItem[] = [
   //{ id: "cafe-latte-12", name: "Coffee latte", price: 7.00, category: "CAFÉ 12oz", description: "Espresso + leche + crema" },
 
   // SIN CAFÉ 12oz
-  { id: "sincafe-choco-milk-12", name: "Choco milk", price: 7.00, category: "SIN CAFÉ 12oz", image: "/chocomilk.webp" },
-  { id: "sincafe-choco-baileys-12", name: "Choco Baileys", price: 10.00, category: "SIN CAFÉ 12oz", image: "/ChocoBAILEYS.webp" },
-  { id: "sincafe-caramel-latte-12", name: "Caramel Latte", price: 7.00, category: "SIN CAFÉ 12oz", image: "/CARAMEL LATTE.webp" },
+  { id: "sincafe-choco-milk-12", name: "Choco milk", price: 7.00, category: "SIN CAFÉ 12oz", image: "/SinCafe/Chocomilk.webp" },
+  { id: "sincafe-choco-baileys-12", name: "Choco Baileys", price: 10.00, category: "SIN CAFÉ 12oz", image: "/SinCafe/ChocoBaileys.webp" },
+  { id: "sincafe-caramel-latte-12", name: "Caramel Latte", price: 7.00, category: "SIN CAFÉ 12oz", image: "/SinCafe/CaramelLatte.webp" },
   //{ id: "sincafe-baileys-latte-12", name: "Baileys Latte", price: 10.00, category: "SIN CAFÉ 12oz" },
-  { id: "trago-lumumba-latte-12", name: "Trago Lumumba", price: 10.00, category: "SIN CAFÉ 12oz", image: "/TRAGO LUMUMBA.webp"},
+  { id: "trago-lumumba-latte-12", name: "Trago Lumumba", price: 10.00, category: "SIN CAFÉ 12oz", image: "/SinCafe/TragoLumumba.webp"},
 
   // FRAPPUCCINOS 12oz
-  { id: "frap-cafe-12", name: "Frappuccino Café", price: 9.00, category: "FRAPPUCCINOS 12oz", image: "/FRAPUCCINO DE CAFE.webp" },
-  { id: "frap-caramel-12", name: "Frappuccino Caramel", price: 9.00, category: "FRAPPUCCINOS 12oz", image: "/frapuccino de caramel.webp" },
-  { id: "frap-moca-12", name: "Frappuccino Moca", price: 9.00, category: "FRAPPUCCINOS 12oz", image: "/frapuccino de moca.webp" },
-  { id: "frap-matcha-12", name: "Frappuccino Matcha", price: 10.00, category: "FRAPPUCCINOS 12oz", image: "/FRAPPUCINO MATCHA.webp" },
+  { id: "frap-cafe-12", name: "Frappuccino Café", price: 9.00, category: "FRAPPUCCINOS 12oz", image: "/Frappucinos/FrappucinoCafe.webp" },
+  { id: "frap-caramel-12", name: "Frappuccino Caramel", price: 9.00, category: "FRAPPUCCINOS 12oz", image: "/Frappucinos/FrappuccinoCaramel.webp" },
+  { id: "frap-moca-12", name: "Frappuccino Moca", price: 9.00, category: "FRAPPUCCINOS 12oz", image: "/Frappucinos/FrappuccinoMoca.webp" },
+  { id: "frap-matcha-12", name: "Frappuccino Matcha", price: 10.00, category: "FRAPPUCCINOS 12oz", image: "/Frappucinos/FrappucinoMatcha.webp" },
 
   // SMOOTHIES 12oz
-  { id: "smooth-fresa-12", name: "Smoothie Fresa", price: 9.00, category: "SMOOTHIES 12oz", image: "/SMOOTHIES DE FRESA.webp"},
-  { id: "smooth-lucuma-12", name: "Smoothie Lúcuma", price: 9.00, category: "SMOOTHIES 12oz", image: "/SMOOTHIES DE LUCUMA.webp"},
-  { id: "smooth-mango-12", name: "Smoothie Mango", price: 9.00, category: "SMOOTHIES 12oz", image: "/SMOOTHIES DE MANGO.webp"},
-  { id: "smooth-arandano-12", name: "Smoothie Arándano", price: 9.00, category: "SMOOTHIES 12oz", image: "/SMOOTHIES DE ARANDANO.webp"},
-  { id: "smooth-frambuesa-12", name: "Smoothie Frambuesa", price: 11.00, category: "SMOOTHIES 12oz", image: "/smothiedeframbuesa.webp" },
+  { id: "smooth-fresa-12", name: "Smoothie Fresa", price: 9.00, category: "SMOOTHIES 12oz", image: "/Smoothies/SmoothiesFresa.webp"},
+  { id: "smooth-frambuesa-12", name: "Smoothie Frambuesa", price: 11.00, category: "SMOOTHIES 12oz", image: "/Smoothies/SmoothiesFrambuesa.webp" },
+  { id: "smooth-lucuma-12", name: "Smoothie Lúcuma", price: 9.00, category: "SMOOTHIES 12oz", image: "/Smoothies/SmoothiesLucuma.webp"},
+  { id: "smooth-mango-12", name: "Smoothie Mango", price: 9.00, category: "SMOOTHIES 12oz", image: "/Smoothies/SmoothiesMango.webp"},
+  { id: "smooth-arandano-12", name: "Smoothie Arándano", price: 9.00, category: "SMOOTHIES 12oz", image: "/Smoothies/SmoothiesArandano.webp"},
+  { id: "smooth-chocolate-12", name: "Smoothie Chocolate", price: 9.00, category: "SMOOTHIES 12oz", image: "/Smoothies/SmoothiesChocolate.webp"},
 
   // FROZEN 12oz
-  { id: "frozen-fresa-12", name: "Frozen Fresa", price: 8.00, category: "FROZEN 12oz", image: "/FROZEN DE FRESA.webp" },
-  { id: "frozen-lucuma-12", name: "Frozen Lúcuma", price: 8.00, category: "FROZEN 12oz", image: "/FROZEN DE LUCUMA.webp" },
-  { id: "frozen-mango-12", name: "Frozen Mango", price: 8.00, category: "FROZEN 12oz", image: "/FROZEN DE MANGO.webp" },
-  { id: "frozen-arandano-12", name: "Frozen Arándano", price: 8.00, category: "FROZEN 12oz", image: "/FROZEN DE ARANDANO.webp" },
-  { id: "frozen-maracuya-12", name: "Frozen Maracuyá", price: 8.00, category: "FROZEN 12oz", image: "/FROZEN DE MARACUYA.webp" },
-  { id: "frozen-frambuesa-12", name: "Frozen Frambuesa", price: 10.00, category: "FROZEN 12oz", image: "/FROZEN DE FRAMBUESA.webp" },
+  { id: "frozen-fresa-12", name: "Frozen Fresa", price: 8.00, category: "FROZEN 12oz", image: "/Frozen/FrozenFresa.webp" },
+  { id: "frozen-lucuma-12", name: "Frozen Lúcuma", price: 8.00, category: "FROZEN 12oz", image: "/Frozen/FrozenLucuma.webp" },
+  { id: "frozen-mango-12", name: "Frozen Mango", price: 8.00, category: "FROZEN 12oz", image: "/Frozen/FrozenMango.webp" },
+  { id: "frozen-arandano-12", name: "Frozen Arándano", price: 8.00, category: "FROZEN 12oz", image: "/Frozen/FrozenArandano.webp" },
+  { id: "frozen-maracuya-12", name: "Frozen Maracuyá", price: 8.00, category: "FROZEN 12oz", image: "/Frozen/FrozenMaracuya.webp" },
+  { id: "frozen-frambuesa-12", name: "Frozen Frambuesa", price: 10.00, category: "FROZEN 12oz", image: "/Frozen/FrozenFrambuesa.webp" },
 
   // SNACKS
-  { id: "snack-sandwich-pollo", name: "Sándwich – Pollo al horno", price: 6.00, category: "SNACKS", image: "/Pollo al horno.webp" },
-  { id: "snack-sandwich-tocino", name: "Sándwich – Tocino ahumado", price: 6.00, category: "SNACKS", image: "/Tocino ahumado .webp" },
-  { id: "snack-crepe-vainilla", name: "Crepe – Vainilla", price: 4.00, category: "SNACKS", image: "/Crepes de Vainilla.webp" },
-  { id: "snack-crepe-maracuya", name: "Crepe – Maracuyá", price: 4.00, category: "SNACKS", image: "/Crepes de Maracuya.webp" },
-  { id: "snack-crepe-fresa", name: "Crepe - Fresa", price: 4.00, category: "SNACKS", image: "/Crepes de Fresa.webp"},
-  { id: "snack-kanelbullar", name: "Kanelbullar", price: 4.00, description: "Rollo de canela sueco", category: "SNACKS", image: "/Kanelbullar.webp" },
-  { id: "snack-chokladbollar", description: "Trufas de chocolate sueco", name: "Chokladbollar", price: 2.00, category: "SNACKS", image: "/Chokladbollar.webp" },
+  { id: "snack-sandwich-pollo", name: "Sándwich – Pollo al horno", price: 6.00, category: "SNACKS", image: "/Snacks/PolloAlHorno.webp" },
+  { id: "snack-sandwich-tocino", name: "Sándwich – Tocino ahumado", price: 6.00, category: "SNACKS", image: "/Snacks/TocinoAhumado.webp" },
+  { id: "snack-crepe-vainilla", name: "Crepe – Vainilla", price: 4.00, category: "SNACKS", image: "/Snacks/CrepesDeVainilla.webp" },
+  { id: "snack-crepe-maracuya", name: "Crepe – Maracuyá", price: 4.00, category: "SNACKS", image: "/Snacks/CrepesDeMaracuya.webp" },
+  { id: "snack-crepe-fresa", name: "Crepe - Fresa", price: 4.00, category: "SNACKS", image: "/Snacks/CrepesDeFresa.webp"},
+  { id: "snack-kanelbullar", name: "Kanelbullar", price: 4.00, description: "Rollo de canela sueco", category: "SNACKS", image: "/Snacks/Kanelbullar.webp" },
+  { id: "snack-chokladbollar", description: "Trufas de chocolate sueco", name: "Chokladbollar", price: 2.00, category: "SNACKS", image: "/Snacks/Chokladbollar.webp" },
 
   // ADICIONALES
-  { id: "adic-shot-baileys", name: "Shot de Baileys", price: 3.00, category: "ADICIONALES", image: "/Shot de Baileys.webp" },
-  { id: "adic-shot-ron", name: "Shot de Ron", price: 2.00, category: "ADICIONALES", image: "/Shot de Ron.webp" },
-  { id: "adic-crema", name: "Crema", price: 2.00, category: "ADICIONALES", image: "/Crema.webp" },
+  { id: "adic-shot-baileys", name: "Shot de Baileys", price: 3.00, category: "ADICIONALES", image: "/Adicionales/ShotDeBaileys.webp" },
+  { id: "adic-shot-ron", name: "Shot de Ron", price: 2.00, category: "ADICIONALES", image: "/Adicionales/ShotDeRon.webp" },
+  { id: "adic-crema", name: "Crema", price: 2.00, category: "ADICIONALES", image: "/Adicionales/Crema.webp" },
 ]
 
 export const getCategoriesInOrder = (): string[] => {
