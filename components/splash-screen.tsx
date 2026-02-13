c157db935c6a51926c26ca6e1ef2f4daa8416c7b
UrielReyess08 <sj77447994@zegelipae.pe>
2026-02-12 22:47:14 -0500

    se disminuyo los segundos de animacion

--- components/splash-screen.tsx
+++ components/splash-screen.tsx
@@ -13,7 +13,7 @@ export function SplashScreen({ onComplete }: SplashScreenProps) {
         const timer = setTimeout(() => {
             setIsVisible(false)
             setTimeout(onComplete, 500)
-        }, 5000)
+        }, 2000)
 
         return () => clearTimeout(timer)
     }, [onComplete])
